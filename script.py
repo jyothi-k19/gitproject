adaef671a51e532b29418051001da89379d2bcb5
jyothi-k19 <ksn.jyothi1905@gmail.com>
2025-07-15 17:01:03 +0200

    added multiplication function

--- script.py
+++ script.py
@@ -2,8 +2,6 @@
 print("Hello, World!")
 
 # This defines a function that adds two numbers.
-
-
 def add_numbers(a, b):
   return a + b
 
@@ -12,3 +10,13 @@ num1 = 5
 num2 = 10
 sum_result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {sum_result}")
+
+
+def multiply_numbers(c, d):
+  return c * d
+
+# Call the function and print the result.
+num3 = 23
+num4 = 48
+mul_result = multiply_numbers(num3, num4)
+print(f"The Multiplication of {num3} and {num4} is: {mul_result}")
