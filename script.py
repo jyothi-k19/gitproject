e20a10e805b9ce47df08ebafa569f75dc4f0fd7e
jyothi-k19 <ksn.jyothi1905@gmail.com>
2025-07-15 16:51:06 +0200

    added inputs

--- script.py
+++ script.py
@@ -5,3 +5,8 @@ def add_numbers(a, b):
   This function takes two numbers as input and returns their sum.
   """
   return a + b
+
+num1 = 5
+num2 = 10
+sum_result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {sum_result}")
