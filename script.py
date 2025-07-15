dc3c30e386b5f457cb7e69042e80c01c14cbb8f5
jyothi-k19 <ksn.jyothi1905@gmail.com>
2025-07-15 16:07:04 +0200

    Removed script
    Modified file.txt in main
    New line
    
    added script

---
+++ script.py
@@ -0,0 +1,14 @@
+# Comments
+print("Hello, World!")
+
+# This defines a function that adds two numbers.
+
+
+def add_numbers(a, b):
+  return a + b
+
+# Call the function and print the result.
+num1 = 5
+num2 = 10
+sum_result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {sum_result}")
